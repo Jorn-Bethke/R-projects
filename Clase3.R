070c026ea87de690393606bf3b959d88a7c3fa76
Jorn <jorn@MacBookPro15.local>
2020-11-26 11:37:04 -0300

    funcion Join

--- Clase3.R
+++ Clase3.R
@@ -1,8 +1,10 @@
 
 library(tidyverse)
 
-Contagiados <- read_csv("https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto19/CasosActivosPorComuna.csv")
+## PIVOT transformando tablas ----
 
+#cargamos los datos
+Contagiados <- read_csv("https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto19/CasosActivosPorComuna.csv")
 
 Contagiados_Long <- Contagiados %>%  
   pivot_longer(cols = starts_with("2020"), names_to = "Fecha", values_to = "Infectados") %>% 
@@ -29,18 +31,38 @@ colnames(warpbreaks)
 
 Lana <- warpbreaks %>%  pivot_wider(names_from = "tension", 
                                     values_from = "breaks",
-                                    values_fn = mean 
-                                    )
+                                    values_fn = mean) %>% 
+  summarise_if(is.numeric, round) #eso redondea sin decimales si se quiere un numero determinado de decimales ~round(.x,2) para dos decimales
+
+#### JOIN uniendo bases de datos-----
+
+#cargar datos
+
+Episodes <- read_csv("https://raw.githubusercontent.com/derek-corcoran-barrios/The_office/master/The_Office_Episodes_per_Character.csv")
+words <- read_csv("https://raw.githubusercontent.com/derek-corcoran-barrios/The_office/master/The_office_Words.csv")
+stop_words <- read_csv("https://raw.githubusercontent.com/derek-corcoran-barrios/The_office/master/stop_words.csv")
+
+# full_join() juunta dos bases de datos que tenga una columna en comun queaddno una nueva base del tammaño de la más grande.
+
+Episodes_words <- full_join( Episodes, words) #Joining, by = "speaker" la consola avisa por cual columna se reaizó el join.
+
+Episodes_top_10 <- Episodes %>%
+  slice_max(order_by = n_episodes, n= 10) %>%
+  left_join(words)
+
+# inner: only rows with matching keys in both x and y
+# left: all rows in x, adding matching columns from 
+# right: all rows in y, adding matching columns from x
 
 
+left <- left_join(Episodes_top_10, words)
 
-
-
-
-
-
-
-
+Top_Words <- left %>% 
+  anti_join(stop_words) %>%  #elimina de la base de datos left todas las filas que tienen el mismo dato de la columna words en stop_words
+  group_by(word) %>% 
+  summarise(n=n()) %>% 
+  ungroup() %>% 
+  slice_max(order_by = n, n = 20)
 
 
 
