c86c500d1cb376e72184a22d41919c77ba1cd3f1
Jorn <jorn@MacBookPro15.local>
2020-11-26 10:20:03 -0300

    Agregue la librelia (kableExtra)

--- Clase2.R
+++ Clase2.R
@@ -1,3 +1,42 @@
 library(tidyverse)
 library(knitr)
 library(kableExtra)
+
+install.packages("pacman")
+
+library(pacman)
+
+plants <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/plants.csv')
+actions <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/actions.csv')
+threats <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/threats.csv')
+
+
+data(msleep)
+
+Tabla <- msleep %>% 
+  group_by(vore) %>% 
+  summarise_at("sleep_total", .funs = list(Mean= mean, SD = sd)) %>%
+  dplyr::filter(!is.na(vore)) %>% 
+  arrange(desc(Mean))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Clase3.R
+++ Clase3.R
@@ -1,2 +1,40 @@
 
 library(tidyverse)
+
+Contagiados <- read_csv("https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto19/CasosActivosPorComuna.csv")
+
+
+Contagiados_Long <- Contagiados %>%  
+  pivot_longer(cols = starts_with("2020"), names_to = "Fecha", values_to = "Infectados") %>% 
+  mutate(Fecha = lubridate::ymd(Fecha), prevalencia = 100000*(Infectados/Poblacion)) %>% #lubdidate :: pasa la fecha desde class character a class Date y creamos la columna preevalencia
+  dplyr::filter(Fecha == max(Fecha), !is.na(`Codigo comuna`))
+
+
+hist(Contagiados_Long$prevalencia)
+
+data("fish_encounters")
+#114x3
+
+unique(fish_encounters$fish) %>%  length() #19
+unique(fish_encounters$station) %>%  length() #11
+
+Para_captura <- fish_encounters %>% pivot_wider(names_from = "station", # me deja los peces como nombre de filas y las station como columnas asi se ve que pes fue capturado en que station
+            values_from = "seen",
+            values_fill = 0   #para remplazar los NA con un valor en particular.
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
